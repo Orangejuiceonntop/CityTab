67ec9f79a893ab34f0073a62acaba619b1beaae1
orangejuiceontop <susselkikijimenez@gmail.com>
2026-07-25 00:34:29 -0500

    Enter key can be used as a search button
    You can search by pressing enter

---
+++ script.js
@@ -0,0 +1,8 @@
+const input = document.getElementById('input');
+
+input.addEventListener("keypress", (event) => {
+    if (event.key == "Enter") {
+        const search = input.value;
+        window.location.href = _blank`https://www.google.com/search?q=${search}`;
+    }
+});
