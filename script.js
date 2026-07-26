9fe6c201d9f2687652b8d4ec947b4f4a58aea978
orangejuiceontop <susselkikijimenez@gmail.com>
2026-07-25 22:34:52 -0500

    Create clock

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ input.addEventListener("keypress", (event) => {
         window.location.href = _blank`https://www.google.com/search?q=${search}`;
     }
 });
+
+// Clock
+function displayTime(){
+    var dateTime = new Date();
+    var hours = dateTime.getHours();
+    var minutes = dateTime.getMinutes();
+
+    if(hours > 12) {
+        hours = hours - 12
+    }
+
+    document.getElementById('hours').innerHTML = hours;
+    document.getElementById('minutes').innerHTML = minutes;
+}
+setInterval(displayTime, 10);
